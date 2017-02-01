55a2a1062d00f06d596622cf3143ed25c36e5cb6
Alberto Castillo Molina <acmxipiron14@gmail.com>
2017-02-01 23:48:10 +0100

    Testeamos todos los métodos

--- DemoGit1/src/test/java/es/egc/Test1.java
+++ DemoGit1/src/test/java/es/egc/Test1.java
@@ -35,4 +35,17 @@ public class Test1
     {
         assertTrue( new MisMatematicas().divide(2,2) == 1 );
     }
+	
+	public void testApp1(){
+		assertTrue( new MisMatematicas().multiplica(2,2) == 4);
+	}
+	
+	public void testApp2(){
+		assertTrue( new MisMatematicas().suma(2,3) == 5);
+	}
+	
+	public void testApp3(){
+		assertTrue( new MisMatematicas().resta(2,2) == 0);
+	}
+	
 }
--- DemoGit1/src/test/java/es/egc/Test2.java
+++ DemoGit1/src/test/java/es/egc/Test2.java
@@ -33,6 +33,18 @@ public class Test2
      */
     public void testApp()
     {
-        assertTrue( new MisMatematicas().multiplica(4,2) == 8 );
+        assertTrue( new MisMatematicas2().multiplica(4,2) == 8 );
     }
+	
+	public void testApp1(){
+		assertTrue( new MisMatematicas2().divide(2.0,2.0) == 1.0);
+	}
+	
+	public void testApp2(){
+		assertTrue(new MisMatematicas2().suma(2.0,2.0) == 4.0);
+	}
+	
+	public void testApp3(){
+		assertTrue(new MisMatematicas2().resta(2.0,1.0) == 1.0);
+	}
 }
