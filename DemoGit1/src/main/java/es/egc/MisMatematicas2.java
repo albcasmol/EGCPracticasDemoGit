5421dc2c9578991978417a9c7d3dc4ad19cac6fd
Alberto Castillo Molina <acmxipiron14@gmail.com>
2017-02-02 00:45:32 +0100

    Provocando un conflicto
    La variable "a" de la clase MisMatematicas2.java en  la funcion "divide"
    ahora se llama "c"

--- DemoGit1/src/main/java/es/egc/MisMatematicas2.java
+++ DemoGit1/src/main/java/es/egc/MisMatematicas2.java
@@ -1,17 +1,19 @@
 package es.egc;
 import org.apache.log4j.*; 
-
 /**
- * DIVISION
- *
+ * Multiplica2
+ * Divide2
+ * Suma2
+ * RaizCuadrada
+ * Resta2
  */
 public class MisMatematicas2 
 {
 	static Logger log=Logger.getLogger(MisMatematicas.class); 
 	
-	public static double divide2(double a, double b){
-		double result= a/b;
-		log.info("Returning "+a+"/"+b+"="+result);
+	public static double divide2(double c, double b){
+		double result= c/b;
+		log.info("Returning "+c+"/"+b+"="+result);
 		return result;
 		
 	}
@@ -26,6 +28,11 @@ public class MisMatematicas2
 		double result = a+b
 		log.info("Returning" +a+ "+" +b+"=" result);
 		return result;
+	}
+	
+	public static double raizCuadrada(double a){
+		double result = Math.sqrt(a);
+		log.info("Returning Squrt("+a+") =" result);
 	}
 	
 	public static double resta2(double a, double b){
